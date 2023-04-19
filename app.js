7f434678a5c2676b921503b9b61c3b5378dfbc65
nurettinyavuz <Nurettinyavuz.js@gmail.com>
2023-04-19 14:20:52 +0300

    Added partials folder and converted to .ejs extensions. Design edits were made.

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+
+
 const app = express();
 
-const path = require('path');
-/* 
-const myLogger =(req,res,next)=>{
-  console.log("Middleware Log 1");
-  next();
-  //Bir sonraki middleware'a gidebilmek için next kullandık 
-  //Yani kullanıcıya request gönderiyoruz ama response yarım kalıyor 
-  //o yüzden next kullanıyoruz yarım kalmaması için   
-}
-*/
+//Template Engine
+app.set ("view engine","ejs");
 
-//Middlewares
-//Static dosyaları public dosyasının içine attık
 app.use(express.static('public'));
 
+//ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+ res.render('index');
 });
-//__dirname değişkeni, uygulamanın bulunduğu dizinin tam yolunu verir.
+app.get('/about', (req, res) => {
+  res.render('about');
+ });
+ app.get('/add', (req, res) => {
+  res.render('add.ejs');
+ });
 
 const port = 3000;
 app.listen(port, () => {
