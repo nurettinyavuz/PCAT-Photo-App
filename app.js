d6d1421a810870de58d16bdb2ca959e59308719a
nurettinyavuz <Nurettinyavuz.js@gmail.com>
2023-05-07 13:36:53 +0300

    We were already getting the title and description from the user, and now I have activated the image import. I keep the images we receive in the file called uploads. I also sorted the newly added image from the most recently added to the first added on the main page, and I made some design adjustments.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+
 const ejs = require('ejs');
 const path = require('path');
+const fs = require('fs');
 const Photo = require('./models/photo');
 
 const app = express();
@@ -15,24 +18,27 @@ mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db', {
 //Template Engine
 app.set('view engine', 'ejs');
 
+//MIDDLEWARES (Mıddlewares'ları Kaydetmek zorundayız)
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true })); //obje olduğu için : kullandık // urlencoded url'deki data'yı okumayı sağlıyor
 app.use(express.json()); //URL'de kullanıcının girdiği veriyi JSON formatına çeviriyor
+app.use(fileUpload());
 
 //ROUTES
-app.get('/', async(req, res) => {
-  const photos=await Photo.find({});//Fotoğrafları yakaladık //asenkron kullanmamızın nedeni işlem bitene kadar beklememesi lazım daha hızlı çalışması için çünkü zaman alan bir işlem
-  res.render('index',{
-    photos
+app.get('/', async (req, res) => {//.sort('-dateCreated') bu kısım yeni ekleneni başa yazar ana sayfada
+  const photos = await Photo.find({}).sort('-dateCreated'); //Fotoğrafları yakaladık //asenkron kullanmamızın nedeni işlem bitene kadar beklememesi lazım daha hızlı çalışması için çünkü zaman alan bir işlem
+  res.render('index', {
+    photos,
   });
 });
 
-app.get('/photos/:id', async(req, res) => {
-// Id'yi kullanarak fotoğrafları birbirinden ayırdık
-const photo=await Photo.findById(req.params.id); //photo.ejs sayfasında <%= photo.title %> burada yazan photo kısmı const photo olarak tanımladığımız için
-res.render('photo',{ //photo yazan kısım photo.ejs sayfasını temsil eder
-  photo
-})
+app.get('/photos/:id', async (req, res) => {
+  // Id'yi kullanarak fotoğrafları birbirinden ayırdık
+  const photo = await Photo.findById(req.params.id); //photo.ejs sayfasında <%= photo.title %> burada yazan photo kısmı const photo olarak tanımladığımız için
+  res.render('photo', {
+    //photo yazan kısım photo.ejs sayfasını temsil eder
+    photo,
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -44,9 +50,30 @@ app.get('/add', (req, res) => {
 
 //photos dediğimiz kısım add.ejs sayfasında yönlendirdiğimiz için (action:"/photos" yazdığımız yer)
 app.post('/photos', async (req, res) => {
+  /* 
+  console.log(req.files.image); //image name adı //express-fileupload'dan dökümandan bak
   await Photo.create(req.body);
   res.redirect('/'); //Kullanıcı mesajı gönderdikten sonra açılacak sayfayı gösterir
+*/
+  const uploadDir = 'Public/uploads';
+  //Klasörün olup olmadığını kontrol ediyoruz eğer klasör yoksa klasör oluşturacak varsa aşağıdan devam edecek
+  if (!fs.existsSync(uploadDir)) { //Public/uploads klasörünün zaten var olup olmadığı kontrol edilir. (! işareti de )
+    fs.mkdirSync(uploadDir); //Klasör yoksa, fs.mkdirSync yöntemi kullanılarak bu klasör oluşturulur
+  }
+
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/Public/uploads/' + uploadeImage.name; //Yüklenen fotoğraflar Public klasöründe uploads dosyası oluşturur ve içine yüklenir, uploadeImage.name ise dosyanın adı oluşturulur
+  //uploads'ın sağına  / işareti koymazsak dosya yolunu bulamaz
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name, //görselin yolunu bildirdik ,sonradan veritabına kaydetmek için
+    });
+    res.redirect('/');
+  });
 });
+
+
 
 const port = 3000;
 app.listen(port, () => {
@@ -55,7 +82,7 @@ app.listen(port, () => {
 
 //package.json dosyasının içinde scriptin içine start ekliyoruz ve npm start dediğimiz nodemon kullanarak çalıştırıyor
 //Nodemon amacı her değişiklikte otomatik olarak kaydediyor ve tekrardan başlatıyor bu da zamandan kazandırıyor
-//request ile response arasında yapılır tüm işlemler (middleware) 
+//request ile response arasında yapılır tüm işlemler (middleware)
 
 // pcat-test-db
 
